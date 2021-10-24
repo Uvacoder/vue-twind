e34aefb7b9a2c68dc04fc444d88ddf4812f6baf4
Leonardo Gruppelli <leonardo.gruppelli@gmail.com>
2021-10-24 16:00:29 -0300

    FEAT: Improving and updating Card and List components.

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -3,6 +3,9 @@ import tw from 'twin.macro'
 
 export const __global = () => injectGlobal`
   body {
-    ${tw`bg-gray-700 text-white`};
+    ${tw`
+      bg-gray-700
+      text-white
+    `};
   }
 `
