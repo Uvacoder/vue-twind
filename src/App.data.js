43beb3bf44e0d5b3e87b8dba314c9f236547cd3e
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2021-10-25 15:57:24 -0300

    FEAT: Add Icon component.

--- src/App.data.js
+++ src/App.data.js
@@ -1,36 +1,36 @@
 export const cards = [
-	{
-		id: 1,
-		title: 'Lorem Ipsum',
-		subtitle: 'lorem ipsum dolor sit amet',
-		content: 'this is a <b>card</b> with content in it.',
-		type: 'warning',
-	},
-	{
-		id: 2,
-		title: 'Lorem Ipsum',
-		subtitle:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis asperiores tempore voluptates facere, architecto aperiam cum ea magni cupiditate perspiciatis in illo provident necessitatibus eum eligendi. Laudantium explicabo unde qui.',
-		content:
-			'Lorem ipsum dolor sit amet consectetur, <b>adipisicing elit</b>. Blanditiis asperiores tempore voluptates facere, architecto aperiam cum ea magni cupiditate perspiciatis in illo provident necessitatibus eum eligendi. Laudantium explicabo unde qui.',
-		type: 'settings',
-	},
-	{
-		id: 3,
-		title: 'Lorem Ipsum',
-		subtitle: 'lorem ipsum dolor sit amet',
-	},
-	{
-		id: 4,
-		title: 'Lorem Ipsum',
-	},
-	{
-		id: 5,
-		title: 'Lorem Ipsum',
-	},
-	{
-		id: 6,
-		title: 'Lorem Ipsum',
-		subtitle: 'lorem ipsum dolor sit amet',
-	},
-]
+  {
+    id: 1,
+    title: "Lorem Ipsum",
+    subtitle: "lorem ipsum dolor sit amet",
+    content: "this is a <b>card</b> with content in it.",
+  },
+  {
+    id: 2,
+    title: "Lorem Ipsum",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis asperiores tempore voluptates facere, architecto aperiam cum ea magni cupiditate perspiciatis in illo provident necessitatibus eum eligendi. Laudantium explicabo unde qui.",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, <b>adipisicing elit</b>. Blanditiis asperiores tempore voluptates facere, architecto aperiam cum ea magni cupiditate perspiciatis in illo provident necessitatibus eum eligendi. Laudantium explicabo unde qui.",
+    icon: "CogIcon",
+  },
+  {
+    id: 3,
+    title: "Lorem Ipsum",
+    subtitle: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: 4,
+    title: "Lorem Ipsum",
+    icon: "UserIcon",
+  },
+  {
+    id: 5,
+    title: "Lorem Ipsum",
+  },
+  {
+    id: 6,
+    title: "Lorem Ipsum",
+    subtitle: "lorem ipsum dolor sit amet",
+  },
+];
--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,54 +1,41 @@
 <template>
-	<div class="card" :class="__card(children)">
-		<div class="card__front">
-			<div class="card__info">
-				<h3>{{ title }}</h3>
-				<h5 v-if="subtitle">{{ subtitle }}</h5>
-			</div>
+  <div class="card" :class="__card(children)">
+    <div class="card__front">
+      <div class="card__info">
+        <h3>{{ title }}</h3>
+        <h5 v-if="subtitle">{{ subtitle }}</h5>
+      </div>
 
-			<i v-if="icon" class="card__icon">
-				<component :is="icon" />
-			</i>
-		</div>
+      <i v-if="icon" class="card__icon">
+        <Icon :icon="icon" />
+      </i>
+    </div>
 
-		<div v-if="children" class="card__back">
-			<slot />
-		</div>
-	</div>
+    <div v-if="children" class="card__back">
+      <slot />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { useSlots } from 'vue'
-import { __card } from './Card.styles'
-import { CogIcon } from '@heroicons/vue/outline'
-import { InformationCircleIcon } from '@heroicons/vue/outline'
-import { ExclamationCircleIcon } from '@heroicons/vue/outline'
-import { UserIcon } from '@heroicons/vue/outline'
+import { useSlots } from "vue";
+import Icon from "~/components/Icon/Icon.vue";
+import { __card } from "./Card.styles";
 
 const props = defineProps({
-	title: {
-		type: String,
-		required: true,
-	},
-	subtitle: {
-		type: String,
-		required: false,
-	},
-	type: {
-		type: String,
-		required: false,
-		validators(value) {
-			return ~['settings', 'info', 'warning', 'user'].indexOf(value)
-		},
-	},
-})
-const slots = useSlots()
-const children = slots.default().find((child) => typeof child.key === 'number')
-const icons = {
-	settings: CogIcon,
-	info: InformationCircleIcon,
-	warning: ExclamationCircleIcon,
-	user: UserIcon,
-}
-const icon = props.type ? icons[props.type] : null
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+  },
+  icon: {
+    type: String,
+    required: false,
+  },
+});
+const slots = useSlots();
+const children = slots.default().find((child) => typeof child.key === "number");
 </script>
